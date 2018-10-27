ad467e955145d322d8ee9e9e6eef6a1c266edd6a
DHclly <335121817@qq.com>
2018-10-27 10:51:48 +0800

    add the base test Signed-off-by: DHclly <335121817@qq.com>

---
+++ IceDog.xUnit.Test/Mocks/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceDog.xUnit.Test.Mocks
+{
+    public class Product
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public virtual Guid Id { get; set; }
+        /// <summary>
+        /// 产品名称
+        /// </summary>
+        public virtual string Name { get; set; }
+        /// <summary>
+        /// 产品分类
+        /// </summary>
+        public virtual string Category { get; set; }
+        /// <summary>
+        /// 是否停产
+        /// </summary>
+        public virtual bool Discontinued { get; set; }
+        /// <summary>
+        /// 生产日期
+        /// </summary>
+        public virtual DateTime ProductionDate { get; set; }
+    }
+}
---
+++ IceDog.xUnit.Test/Mocks/ProductList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceDog.xUnit.Test.Mocks
+{
+    public class ProductList : List<Product>
+    {
+        public ProductList()
+        {
+            Product[] _products = {
+            new Product {Name = "Melon", Category = "Fruits",Discontinued=false,ProductionDate=new DateTime(2000,5,10)},
+            new Product {Name = "Pear", Category = "Fruits",Discontinued=false,ProductionDate=new DateTime(2001,5,10)},
+            new Product {Name = "Milk", Category = "Beverages",Discontinued=true,ProductionDate=new DateTime(2002,5,10)},
+            new Product {Name = "Coca Cola", Category = "Beverages",Discontinued=true,ProductionDate=new DateTime(2003,5,10)},
+            new Product {Name = "Pepsi Cola", Category = "Beverages",Discontinued=true,ProductionDate=new DateTime(2004,5,10)}
+            };
+            this.AddRange(_products);
+        }
+    }
+}
